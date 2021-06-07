834273fc90cae92dadd4879aa87e6941f32f467c
skywardly <skywandou@gmail.com>
2021-06-07 18:08:47 +0900

    Add template with Vue

--- apiv1/views.py
+++ apiv1/views.py
@@ -11,6 +11,6 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     """本モデルのCRUD用APIクラス"""
 
-    queryset = Book.objecfts.all()
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
